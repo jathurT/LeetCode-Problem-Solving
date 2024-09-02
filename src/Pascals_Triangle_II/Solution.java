9c0ee4107edd215be491ebb31221c16288868ea3
jathurT <jathurshan_t_e23@engug.ruh.ac.lk>
2024-09-02 07:08:44 +0530

    119. Pascal's Triangle II Solution Updated

--- src/Pascals_Triangle_II/Solution.java
+++ src/Pascals_Triangle_II/Solution.java
@@ -1,9 +1,18 @@
 package Pascals_Triangle_II;
 
+import java.util.ArrayList;
 import java.util.List;
 
 class Solution {
   public List<Integer> getRow(int rowIndex) {
-
+    List<Integer> row = new ArrayList<>();
+    row.add(0, 1);
+    for (int i = 1; i <= rowIndex; i++) {
+      for (int j = i - 1; j > 0; j--) {
+        row.set(j, row.get(j) + row.get(j - 1));
+      }
+      row.add(1);
+    }
+    return row;
   }
 }
