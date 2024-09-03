4187e58faf8a471f10a7e8ccf7a5a5cb5b2e4a5d
jathurT <jathurshan_t_e23@engug.ruh.ac.lk>
2024-09-03 06:30:05 +0530

    121. Best Time to Buy and Sell Stock Solution Updated

---
+++ src/Best_Time_to_Buy_and_Sell_Stock/Solution.java
@@ -0,0 +1,19 @@
+package Best_Time_to_Buy_and_Sell_Stock;
+
+// Time complexity: O(n)
+class Solution {
+  public int maxProfit(int[] prices) {
+    int minPrice = Integer.MAX_VALUE;
+    int maxProfit = 0;
+
+    for (int price : prices) {
+      if (price < minPrice) {
+        minPrice = price;
+      } else if (price - minPrice > maxProfit) {
+        maxProfit = price - minPrice;
+      }
+    }
+
+    return maxProfit;
+  }
+}
