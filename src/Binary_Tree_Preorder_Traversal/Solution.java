df86c9bdf38537bd3234ca7597be3365fbcc1018
jathurT <jathurshan_t_e23@engug.ruh.ac.lk>
2024-09-11 16:13:26 +0530

    144. Binary Tree Preorder Traversal Solution Updated

---
+++ src/Binary_Tree_Preorder_Traversal/Solution.java
@@ -0,0 +1,30 @@
+package Binary_Tree_Preorder_Traversal;
+
+
+import java.util.List;
+
+//Definition for a binary tree node.
+class TreeNode {
+  int val;
+  TreeNode left;
+  TreeNode right;
+
+  TreeNode() {
+  }
+
+  TreeNode(int val) {
+    this.val = val;
+  }
+
+  TreeNode(int val, TreeNode left, TreeNode right) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
+}
+
+class Solution {
+  public List<Integer> preorderTraversal(TreeNode root) {
+
+  }
+}
