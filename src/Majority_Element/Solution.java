7dc2561bdb23cc7597cfc9334be0cd4d1b1fe69c
jathurT <jathurshan_t_e23@engug.ruh.ac.lk>
2024-10-02 14:16:52 +0530

    169. Majority Element Solution Updated

--- src/Majority_Element/Solution.java
+++ src/Majority_Element/Solution.java
@@ -1,5 +1,8 @@
 package Majority_Element;
 
+import java.util.HashMap;
+import java.util.Map;
+
 // Boyer-Moore Voting Algorithm
 //The main idea is that non-majority elements are "canceled out" by majority
 // elements. Because the majority element appears more than n / 2 times,
@@ -16,5 +19,25 @@ class Solution {
       count += (num == candidate) ? 1 : -1;
     }
     return candidate;
+  }
+}
+
+// Time Complexity: O(n)
+class Solution1 {
+  public int majorityElement(int[] nums) {
+    Map<Integer, Integer> map = new HashMap<>();
+    for (int num : nums) {
+      if (map.containsKey(num)) {
+        map.put(num, map.get(num) + 1);
+      } else {
+        map.put(num, 1);
+      }
+    }
+    for (int num : nums) {
+      if (map.get(num) > nums.length / 2) {
+        return num;
+      }
+    }
+    return -1;
   }
 }
