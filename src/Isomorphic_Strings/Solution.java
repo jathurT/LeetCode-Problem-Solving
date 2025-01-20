25e628bf5113517fec8b5aa0bc3ad00a9e9eaea3
jathurT <jathurshan_t_e23@engug.ruh.ac.lk>
2025-01-20 07:12:45 +0530

    205. Isomorphic Strings Solution Added

--- src/Isomorphic_Strings/Solution.java
+++ src/Isomorphic_Strings/Solution.java
@@ -6,8 +6,8 @@ import java.util.List;
 import java.util.Map;
 
 class Solution {
-//  Approach 1
-//  public boolean isIsomorphic(String s, String t) {
+  //  Approach 1
+  public boolean isIsomorphic(String s, String t) {
 /// /    Approach 1.1
 /// /    Consume the runtime less
 //
@@ -24,45 +24,43 @@ class Solution {
 //    }
 //    return true;
 
-  /// /   Approach 1.2
-  /// /    Consume the runtime more than 1.1 as it uses List
-//
-//    List<Integer> sMap = new ArrayList<>();
-//      List<Integer> tMap = new ArrayList<>();
-//      for (int i = 0; i < s.length(); i++) {
-//        char sChar = s.charAt(i);
-//        char tChar = t.charAt(i);
-//        if (sMap.indexOf((int) sChar) != tMap.indexOf((int) tChar)) {
-//          return false;
-//        }
-//        sMap.add((int) sChar);
-//        tMap.add((int) tChar);
-//
-//    }
-//    return true;
-//}
+    /// /   Approach 1.2
+    /// /    Consume the runtime more than 1.1 as it uses List
+    List<Character> sMap = new ArrayList<>();
+    List<Character> tMap = new ArrayList<>();
+    for (int i = 0; i < s.length(); i++) {
+      char sChar = s.charAt(i);
+      char tChar = t.charAt(i);
+      if (sMap.indexOf(sChar) != tMap.indexOf(tChar)) {
+        return false;
+      }
+      sMap.add(sChar);
+      tMap.add(tChar);
+    }
+    return true;
+  }
 
 
 //  Approach 2
 // Using Mapping
-  public boolean isIsomorphic(String s, String t) {
-    return isIsomorphicHelper(s, t) && isIsomorphicHelper(t, s);
-  }
-
-  public boolean isIsomorphicHelper(String s, String t){
-    Map<Character, Character> sMap = new HashMap<>();
-    for (int i = 0; i < s.length(); i++) {
-      char sChar = s.charAt(i);
-      char tChar = t.charAt(i);
-      if (sMap.containsKey(sChar)) {
-        if (sMap.get(sChar) != tChar) {
-          return false;
-        }
-      } else {
-        sMap.put(sChar, tChar);
-      }
-    }
-    return true;
-  }
+//  public boolean isIsomorphic(String s, String t) {
+//    return isIsomorphicHelper(s, t) && isIsomorphicHelper(t, s);
+//  }
+//
+//  public boolean isIsomorphicHelper(String s, String t){
+//    Map<Character, Character> sMap = new HashMap<>();
+//    for (int i = 0; i < s.length(); i++) {
+//      char sChar = s.charAt(i);
+//      char tChar = t.charAt(i);
+//      if (sMap.containsKey(sChar)) {
+//        if (sMap.get(sChar) != tChar) {
+//          return false;
+//        }
+//      } else {
+//        sMap.put(sChar, tChar);
+//      }
+//    }
+//    return true;
+//  }
 
 }
