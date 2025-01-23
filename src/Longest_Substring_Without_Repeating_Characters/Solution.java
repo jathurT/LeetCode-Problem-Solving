59ba286ef6208bd4be07c7abffe117bd2fd9c4f5
jathurT <jathurshan_t_e23@engug.ruh.ac.lk>
2025-01-23 06:48:55 +0530

    3. Longest Substring Without Repeating Characters Solution Added

--- src/Longest_Substring_Without_Repeating_Characters/Solution.java
+++ src/Longest_Substring_Without_Repeating_Characters/Solution.java
@@ -1,24 +1,32 @@
 package Longest_Substring_Without_Repeating_Characters;
 
-//class Solution {
-//  public int lengthOfLongestSubstring(String s) {
-//    int n = s.length();
-//    int ans = 0;
-//    int[] index = new int[128];
-//    for (int j = 0, i = 0; j < n; j++) {
-//      i = Math.max(index[s.charAt(j)], i);
-//      ans = Math.max(ans, j - i + 1);
-//      index[s.charAt(j)] = j + 1;
-//    }
-//    return ans;
-//  }
-//}
-
-
 import java.util.HashSet;
 import java.util.Set;
 
-/// / Naive Approach - Consider Substrings Starting From Every Index
+class Solution {
+  public int lengthOfLongestSubstring(String s) {
+    int n = s.length();
+    int ans = 0;
+    int[] index = new int[128];
+    for (int j = 0, i = 0; j < n; j++) {
+      i = Math.max(index[s.charAt(j)], i);
+      ans = Math.max(ans, j - i + 1);
+      index[s.charAt(j)] = j + 1;
+    }
+    return ans;
+  }
+
+  public static void main(String[] args) {
+    Solution solution = new Solution();
+    System.out.println(solution.lengthOfLongestSubstring("abcabcbb")); // 3
+  }
+}
+
+
+//import java.util.HashSet;
+//import java.util.Set;
+
+//// Naive Approach - Consider Substrings Starting From Every Index
 //class Solution {
 //  public int lengthOfLongestSubstring(String s) {
 //    int n = s.length();
@@ -29,32 +37,32 @@ import java.util.Set;
 //        if (visited.contains(s.charAt(j))) {
 //          break;
 //        } else {
-//          res = Math.max(res, j - i + 1);
 //          visited.add(s.charAt(j));
 //        }
 //      }
+//      res = Math.max(res, visited.size());
 //    }
 //    return res;
 //  }
 //}
 
 
-// Optimized Sliding Window Approach
-class Solution {
-  public int lengthOfLongestSubstring(String s) {
-    int n = s.length();
-    int res = 0;
-    Set<Character> visited = new HashSet<>();
-    int left = 0, right = 0;
-    while (right < n) {
-      while (visited.contains(s.charAt(right))) {
-        visited.remove(s.charAt(left));
-        left++;
-      }
-      visited.add(s.charAt(right));
-      res = Math.max(res, right - left + 1);
-      right++;
-    }
-    return res;
-  }
-}
+//// Optimized Sliding Window Approach
+//class Solution {
+//  public int lengthOfLongestSubstring(String s) {
+//    int n = s.length();
+//    int res = 0;
+//    Set<Character> visited = new HashSet<>();
+//    int left = 0, right = 0;
+//    while (right < n) {
+//      while (visited.contains(s.charAt(right))) {
+//        visited.remove(s.charAt(left));
+//        left++;
+//      }
+//      visited.add(s.charAt(right));
+//      res = Math.max(res, right - left + 1);
+//      right++;
+//    }
+//    return res;
+//  }
+//}
