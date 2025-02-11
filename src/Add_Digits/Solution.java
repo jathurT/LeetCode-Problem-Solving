5ce8a36c39bda3015ff41fe16f53959620064725
jathurT <jathurshan_t_e23@engug.ruh.ac.lk>
2025-02-11 06:17:12 +0530

    258. Add Digits Solution Added

--- src/Add_Digits/Solution.java
+++ src/Add_Digits/Solution.java
@@ -10,3 +10,24 @@ class Solution {
     System.out.println(solution.addDigits(38));
   }
 }
+
+
+// Approach 1 - Iterative
+//class Solution {
+//  public int addDigits(int num) {
+//    while (num >= 10) {
+//      int sum = 0;
+//      while (num != 0) {
+//        sum += num % 10;
+//        num /= 10;
+//      }
+//      num = sum;
+//    }
+//    return num;
+//  }
+//
+//  public static void main(String[] args) {
+//    Solution solution = new Solution();
+//    System.out.println(solution.addDigits(38));
+//  }
+//}
